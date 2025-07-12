f0bd71704b9a28578acb1993e14808fd6d8dc620
sumitmalik5 <sumitmalik29@gmail.com>
2025-07-12 07:20:32 +0000

    add git automate ci push

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to Python Flask World v7.0 from cicd'
+    return 'Welcome to Python Flask World v7.0 from cicd git push automate'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
